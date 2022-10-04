72ce53a3d4bf5042aa7f3b22b8cf18dba765f76c
Daniel Alfonso Peña Quintero <danyalf888@gmail.com>
2022-10-04 15:17:28 -0500

    Proyecto reto2

---
+++ Reto2_BibliotecaEAN/Registro de libros EAN.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace Reto2_BibliotecaEAN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modificarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.text_buscar = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.text_codigo = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.text_cantidad = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.text_precio = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.text_editorial = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.text_autor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.text_titulo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoToolStripMenuItem,
+            this.registrarToolStripMenuItem,
+            this.modificarToolStripMenuItem,
+            this.eliminarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1001, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(71, 24);
+            this.nuevoToolStripMenuItem.Text = "Nuevo";
+            this.nuevoToolStripMenuItem.Click += new System.EventHandler(this.nuevoToolStripMenuItem_Click);
+            // 
+            // registrarToolStripMenuItem
+            // 
+            this.registrarToolStripMenuItem.Name = "registrarToolStripMenuItem";
+            this.registrarToolStripMenuItem.Size = new System.Drawing.Size(96, 24);
+            this.registrarToolStripMenuItem.Text = "Registrar";
+            this.registrarToolStripMenuItem.Click += new System.EventHandler(this.registrarToolStripMenuItem_Click);
+            // 
+            // modificarToolStripMenuItem
+            // 
+            this.modificarToolStripMenuItem.Name = "modificarToolStripMenuItem";
+            this.modificarToolStripMenuItem.Size = new System.Drawing.Size(101, 24);
+            this.modificarToolStripMenuItem.Text = "Modificar";
+            this.modificarToolStripMenuItem.Click += new System.EventHandler(this.modificarToolStripMenuItem_Click);
+            // 
+            // eliminarToolStripMenuItem
+            // 
+            this.eliminarToolStripMenuItem.Name = "eliminarToolStripMenuItem";
+            this.eliminarToolStripMenuItem.Size = new System.Drawing.Size(95, 24);
+            this.eliminarToolStripMenuItem.Text = "Eliminar";
+            this.eliminarToolStripMenuItem.Click += new System.EventHandler(this.eliminarToolStripMenuItem_Click);
+            // 
+            // text_buscar
+            // 
+            this.text_buscar.BackColor = System.Drawing.Color.MintCream;
+            this.text_buscar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.text_buscar.Location = new System.Drawing.Point(305, 29);
+            this.text_buscar.Name = "text_buscar";
+            this.text_buscar.Size = new System.Drawing.Size(267, 29);
+            this.text_buscar.TabIndex = 1;
+            this.text_buscar.TextChanged += new System.EventHandler(this.text_buscar_TextChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.SystemColors.Info;
+            this.label10.Location = new System.Drawing.Point(229, 29);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(65, 22);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Titulo";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Green;
+            this.groupBox3.Controls.Add(this.text_buscar);
+            this.groupBox3.Controls.Add(this.label10);
+            this.groupBox3.Controls.Add(this.dataGridView1);
+            this.groupBox3.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.ForeColor = System.Drawing.SystemColors.Info;
+            this.groupBox3.Location = new System.Drawing.Point(28, 247);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(940, 378);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Mostrar libros";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Linen;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.Desktop;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.ControlDarkDark;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(14, 64);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(912, 300);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.ForestGreen;
+            this.groupBox1.BackgroundImage = global::Reto2_BibliotecaEAN.Properties.Resources.imgEan1;
+            this.groupBox1.Controls.Add(this.text_codigo);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.text_cantidad);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.text_precio);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.text_editorial);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.text_autor);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.text_titulo);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.groupBox1.Location = new System.Drawing.Point(28, 53);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(940, 165);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ménu de ingreso de datos";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // text_codigo
+            // 
+            this.text_codigo.BackColor = System.Drawing.Color.MintCream;
+            this.text_codigo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.text_codigo.Enabled = false;
+            this.text_codigo.Location = new System.Drawing.Point(746, 105);
+            this.text_codigo.Name = "text_codigo";
+            this.text_codigo.Size = new System.Drawing.Size(100, 26);
+            this.text_codigo.TabIndex = 11;
+            this.text_codigo.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.SeaGreen;
+            this.label6.ForeColor = System.Drawing.SystemColors.Info;
+            this.label6.Location = new System.Drawing.Point(680, 107);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Código";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // text_cantidad
+            // 
+            this.text_cantidad.BackColor = System.Drawing.Color.MintCream;
+            this.text_cantidad.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.text_cantidad.Location = new System.Drawing.Point(449, 101);
+            this.text_cantidad.Name = "text_cantidad";
+            this.text_cantidad.Size = new System.Drawing.Size(100, 26);
+            this.text_cantidad.TabIndex = 9;
+            this.text_cantidad.TextChanged += new System.EventHandler(this.text_cantidad_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.SeaGreen;
+            this.label5.ForeColor = System.Drawing.SystemColors.Info;
+            this.label5.Location = new System.Drawing.Point(352, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Cantidad";
+            // 
+            // text_precio
+            // 
+            this.text_precio.BackColor = System.Drawing.Color.MintCream;
+            this.text_precio.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.text_precio.Location = new System.Drawing.Point(165, 109);
+            this.text_precio.Name = "text_precio";
+            this.text_precio.Size = new System.Drawing.Size(100, 26);
+            this.text_precio.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.SeaGreen;
+            this.label4.ForeColor = System.Drawing.SystemColors.Info;
+            this.label4.Location = new System.Drawing.Point(99, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Precio";
+            // 
+            // text_editorial
+            // 
+            this.text_editorial.BackColor = System.Drawing.Color.MintCream;
+            this.text_editorial.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.text_editorial.Location = new System.Drawing.Point(718, 26);
+            this.text_editorial.Name = "text_editorial";
+            this.text_editorial.Size = new System.Drawing.Size(155, 26);
+            this.text_editorial.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.SeaGreen;
+            this.label3.ForeColor = System.Drawing.SystemColors.Info;
+            this.label3.Location = new System.Drawing.Point(631, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Editorial";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // text_autor
+            // 
+            this.text_autor.BackColor = System.Drawing.Color.MintCream;
+            this.text_autor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.text_autor.Location = new System.Drawing.Point(372, 28);
+            this.text_autor.Name = "text_autor";
+            this.text_autor.Size = new System.Drawing.Size(166, 26);
+            this.text_autor.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.SeaGreen;
+            this.label2.ForeColor = System.Drawing.SystemColors.Info;
+            this.label2.Location = new System.Drawing.Point(311, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Autor";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // text_titulo
+            // 
+            this.text_titulo.BackColor = System.Drawing.Color.MintCream;
+            this.text_titulo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.text_titulo.Location = new System.Drawing.Point(103, 30);
+            this.text_titulo.Name = "text_titulo";
+            this.text_titulo.Size = new System.Drawing.Size(151, 26);
+            this.text_titulo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.SeaGreen;
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(40, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Título";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1001, 637);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Registro de libros EAN";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nuevoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registrarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modificarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox text_editorial;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox text_autor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox text_titulo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox text_cantidad;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox text_precio;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox text_buscar;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox text_codigo;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Reto2_BibliotecaEAN/Registro de libros EAN.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Capa_Entidad;
+using Capa_Negocio;
+
+namespace Reto2_BibliotecaEAN
+{
+    public partial class Form1 : Form
+    {
+        ClassEntidad objent = new ClassEntidad();
+        ClassNegocio objneg = new ClassNegocio();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void text_buscar_TextChanged(object sender, EventArgs e)
+        {
+            if (text_buscar.Text != "")
+            {
+                objent.titulo = text_buscar.Text;
+                DataTable dt = new DataTable();
+                dt = objneg.N_buscar_libros(objent);
+                dataGridView1.DataSource = dt;
+            }
+            else
+            {
+                dataGridView1.DataSource = objneg.N_listar_libros();
+            }
+        }
+
+        void mantenimiento(String accion)
+        {
+            
+            
+                objent.codigo = Convert.ToInt32(text_codigo.Text);
+                objent.titulo = text_titulo.Text;
+                objent.autor = text_autor.Text;
+                objent.editorial = text_editorial.Text;
+                objent.precio = Convert.ToDouble(text_precio.Text);
+                objent.cantidad = Convert.ToInt32(text_cantidad.Text);
+                objent.accion = accion;
+                String men = objneg.N_mantenimiento_libro(objent);
+                MessageBox.Show(men, "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            
+        }
+
+
+        void limpiar()
+        {
+            text_codigo.Text = "0";
+            text_titulo.Text = "";
+            text_autor.Text = "";
+            text_editorial.Text = "";
+            text_precio.Text = "";
+            text_cantidad.Text = "";
+            text_buscar.Text = "";
+            dataGridView1.DataSource = objneg.N_listar_libros();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            text_codigo.Text = "0";
+            dataGridView1.DataSource = objneg.N_listar_libros();
+        }
+
+        private void nuevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            limpiar();
+        }
+
+        private void registrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (text_titulo.Text != "") {
+                if (MessageBox.Show("¿Deseas registrar el libro " + text_titulo.Text + "?", "Mensaje",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    mantenimiento("1");
+                    limpiar();
+                }
+            }
+           
+            
+        }
+
+        private void modificarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (text_codigo.Text != "")
+            {
+                if (MessageBox.Show("¿Deseas modificar el libro " + text_titulo.Text + "?", "Mensaje",
+                                MessageBoxButtons.YesNo, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    mantenimiento("2");
+                    limpiar();
+                }
+            }
+                
+        }
+
+        private void eliminarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (text_codigo.Text != "")
+            {
+                if (MessageBox.Show("¿Deseas eliminar el libro " + text_titulo.Text + "?", "Mensaje",
+                                MessageBoxButtons.YesNo, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    mantenimiento("3");
+                    limpiar();
+                }
+            }
+                
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int fila = dataGridView1.CurrentCell.RowIndex;
+
+            text_codigo.Text = dataGridView1[0, fila].Value.ToString();
+            text_titulo.Text = dataGridView1[1, fila].Value.ToString();
+            text_autor.Text = dataGridView1[2, fila].Value.ToString();
+            text_editorial.Text = dataGridView1[3, fila].Value.ToString();
+            text_precio.Text = dataGridView1[4, fila].Value.ToString();
+            text_cantidad.Text = dataGridView1[5, fila].Value.ToString();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void text_cantidad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
